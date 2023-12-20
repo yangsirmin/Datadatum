7a72a578387f2a60fbea19e859871f8ccbdc146a
ybc <2309300728@qq.com>
2023-12-20 21:23:31 +0800

    整理

--- src/main/java/org/example/table/DesignSingleTable.java
+++ src/main/java/org/example/table/DesignSingleTable.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.table;
 
 public class DesignSingleTable {
     public static void main(String[] args) {
--- src/main/java/org/example/table/ReversalTable.java
+++ src/main/java/org/example/table/ReversalTable.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.example.table;
 
 public class ReversalTable {
 }
