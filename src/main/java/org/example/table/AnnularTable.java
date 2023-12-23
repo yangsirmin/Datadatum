8acaceeed466eff0bed449002befb8e987486f06
ybc <2309300728@qq.com>
2023-12-23 11:46:38 +0800

    有效的字母异位词

--- src/main/java/org/example/table/AnnularTable.java
+++ src/main/java/org/example/table/AnnularTable.java
@@ -6,7 +6,7 @@ public class AnnularTable {
  * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。
  * 如果链表无环，则返回 null。
  */
-public class Solution10 {
+class Solution10 {
     public ListNode detectCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
