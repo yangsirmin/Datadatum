1b1d1da1bf3eccf34f91c794c8bb43b684e24776
ybc <2309300728@qq.com>
2023-12-23 11:17:43 +0800

    环形链表

---
+++ src/main/java/org/example/table/AnnularTable.java
@@ -0,0 +1,29 @@
+package org.example.table;
+
+public class AnnularTable {
+}
+/**
+ * 给定一个链表的头节点  head ，返回链表开始入环的第一个节点。
+ * 如果链表无环，则返回 null。
+ */
+public class Solution10 {
+    public ListNode detectCycle(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        ListNode index1, index2;
+        while (fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if (fast == slow){
+                index1 = fast;
+                index2 = head;
+                while (index1 != index2){
+                    index1 = index1.next;
+                    index2 = index2.next;
+                }
+                return index1;
+            }
+        }
+        return null;
+    }
+}
