4f3f7553a613eb005ad83d803753f5ef8588c41a
ybc <2309300728@qq.com>
2024-04-24 14:52:49 +0800

    回溯-组合

--- src/main/java/org/example/binaryTree/SearchToAddTree.java
+++ src/main/java/org/example/binaryTree/SearchToAddTree.java
@@ -5,7 +5,7 @@ public class SearchToAddTree {
      * 给出二叉 搜索 树的根节点，该树的节点值各不相同，请你将其转换为累加树（Greater Sum Tree），
      * 使每个节点 node 的新值等于原树中大于或等于 node.val 的值之和。
      */
-    class Solution {
+    class Solution58 {
         int pre = 0;
         public TreeNode convertBST(TreeNode root) {
             if (root == null){
